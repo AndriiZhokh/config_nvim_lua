4190e66a8a1b51b42391ffa107ddf68798dffe06
Andrii Zhokh <andriizhokh@gmail.com>
2022-05-08 21:09:58 +0300

    added lsp server support by chrisatmachine video

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -61,10 +61,16 @@ return packer.startup(function(use)
   use "hrsh7th/cmp-path" -- path completions
   use "hrsh7th/cmp-cmdline" -- cmdline completions
   use "saadparwaiz1/cmp_luasnip" -- snippet completions
+  use "hrsh7th/cmp-nvim-lsp" -- lsp autocompletion
+  use "hrsh7th/cmp-nvim-lua" -- lua autocompletion
 
   -- snippets
   use "L3MON4D3/LuaSnip" -- snippet engine
   use "rafamadriz/friendly-snippets" -- a bunch of snippers to use
+  
+  -- LuaSnip
+  use "neovim/nvim-lspconfig" -- enable LSP
+  use "williamboman/nvim-lsp-installer" -- simple to use language sever installer
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
@@ -72,3 +78,4 @@ return packer.startup(function(use)
     require("packer").sync()
   end
 end)
+
