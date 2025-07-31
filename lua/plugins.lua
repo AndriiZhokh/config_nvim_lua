d58660ca5ba10b368a2ba3593cf0a1963037143f
Andrii Zhokh <andriizhokh@gmail.com>
2025-07-31 10:24:46 +0300

    updated pachages and added todo items

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,12 +1,12 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    'git',
+    'clone',
+    '--filter=blob:none',
+    'https://github.com/folke/lazy.nvim.git',
+    '--branch=stable', -- latest stable release
     lazypath,
   })
 end
@@ -22,8 +22,9 @@ local plugins = {
   require 'plugins.telescope',
   require 'plugins.zen-mode',
 
-  -- TODO: to check
+  -- TODO: to check new plugins
   -- https://github.com/krshrimali/context-pilot.nvim - git helper plugin
+  -- https://github.com/folke/flash.nvim - for better navigation in file
 }
 
-require("lazy").setup(plugins)
+require('lazy').setup(plugins)
