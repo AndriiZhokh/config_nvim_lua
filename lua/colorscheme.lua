4ce7c55e22fab59b374410a3a453d6a128ae4a2f
Andrii Zhokh <andriizhokh@gmail.com>
2025-10-27 09:21:46 +0200

    changed selected colorscheme

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,6 +1,6 @@
-local colorscheme = 'everforest'
+-- local colorscheme = 'everforest'
 -- local colorscheme = 'yugen'
--- local colorscheme = 'nord'
+local colorscheme = 'nord'
 
 local status_ok, _ = pcall(vim.cmd, 'colorscheme ' .. colorscheme)
 
